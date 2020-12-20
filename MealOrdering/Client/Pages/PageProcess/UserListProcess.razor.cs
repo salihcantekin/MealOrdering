19df92b77bfe48f0f4a3a024455d1ca84526f6d7
Salih Cantekin <salih.cantekin@etstur.com>
2020-12-20 23:57:33 +0300

    user data listed on Userlist.razor as

---
+++ MealOrdering/Client/Pages/PageProcess/UserListProcess.razor.cs
@@ -0,0 +1,42 @@
+﻿using MealOrdering.Shared.DTO;
+using MealOrdering.Shared.ResponseModels;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace MealOrdering.Client.Pages.Users
+{
+    public class UserListProcess: ComponentBase
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+
+        protected List<UserDTO> userList = new List<UserDTO>();
+
+
+
+
+
+        protected async override Task OnInitializedAsync()
+        {
+            await LoadList();
+        }
+
+
+        protected async Task LoadList()
+        {
+            var serviceResponse = await Client.GetFromJsonAsync<ServiceResponse<List<UserDTO>>>("api/User/Users");
+
+            if (serviceResponse.Success)
+                userList = serviceResponse.Value;
+        }
+
+
+
+    }
+}
